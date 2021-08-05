f3d20a22ebafdb289b8c7c2fbea5c02b4bc42c35
celensercan <sercancelenk34@hotmail.com>
2021-08-05 23:14:58 +0300

    invalid transaction

--- src/blockchain.js
+++ src/blockchain.js
@@ -90,7 +90,15 @@ class Blockchain{
         ];
     }
 
-    createTransaction(transaction){
+    addTransaction(transaction){
+        if(!transaction.fromAddress || transaction.toAddress){
+            throw new Error('Transaction must be include from and to address');
+        }
+
+        if(!transaction.isValid()){
+            throw new Error('Cannot add invalid transaction to chain');
+
+        }
         this.pendingTransactions.push(transaction);
     }
     getBalanceOfAddress(address){
