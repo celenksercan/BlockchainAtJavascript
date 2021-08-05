b5d0fd7e6896db4348b79d56fe57a9c7cfaf6672
celensercan <sercancelenk34@hotmail.com>
2021-08-05 23:23:08 +0300

    add private key and wallet adress

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const {Blockchain, Transaction}=require('./blockchain');
 const EC=require('elliptic').ec;
 const ec=new EC('secp256k1');
 
-
+const myKey=ec.keyFromPrivate('c3d3e71dc23e73e0a65657fb03e153738e8bbce5dc5e15e624bbf63d055122ff');
+const myWalletAddress=myKey.getPublic('hex');
 
 let Bakor=new Blockchain();
 Bakor.createTransaction(new Transaction('address1','address2',100));
