27b91df1a3ccbfee663a67e99523f65a638327e6
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-03 22:52:57 +0300

    Update transaction.test.js

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -1,3 +1,9 @@
 const assert = require('assert');
 const { Transaction } = require('../src/blockchain');
 const { createSignedTx, signingKey } = require('./helpers');
+
+let txObject = null;
+
+beforeEach(function() {
+  txObject = new Transaction('fromAddress', 'toAddress', 9999);
+});
