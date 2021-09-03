efb31812366fa7f165775b9f40c29c3c4b6136a3
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-03 22:53:09 +0300

    Update transaction.test.js

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -7,3 +7,10 @@ let txObject = null;
 beforeEach(function() {
   txObject = new Transaction('fromAddress', 'toAddress', 9999);
 });
+
+describe('Transaction class', function() {
+  describe('Constructor', function() {
+    it('should automatically set the current date', function() {
+      const actual = txObject.timestamp;
+      const minTime = Date.now() - 1000;
+      const maxTime = Date.now() + 1000;
