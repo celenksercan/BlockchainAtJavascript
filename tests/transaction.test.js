a70c3cd738fdd42e8037dd70a5cbcd3eb7dcdbec
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-03 22:53:20 +0300

    Update transaction.test.js

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -14,3 +14,16 @@ describe('Transaction class', function() {
       const actual = txObject.timestamp;
       const minTime = Date.now() - 1000;
       const maxTime = Date.now() + 1000;
+      
+      assert(actual > minTime && actual < maxTime, 'Tx does not have a good timestamp');
+    });
+
+
+    it('should correctly save from, to and amount', function() {
+      txObject = new Transaction('a1', 'b1', 10);
+
+      assert.strict.equal(txObject.fromAddress, 'a1');
+      assert.strict.equal(txObject.toAddress, 'b1');
+      assert.strict.equal(txObject.amount, 10);
+    });
+  });
