74a2e62d7f97b7aa4f39db0dc6b2aec11334a855
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-03 22:53:41 +0300

    Update transaction.test.js

--- tests/transaction.test.js
+++ tests/transaction.test.js
@@ -27,3 +27,16 @@ describe('Transaction class', function() {
       assert.strict.equal(txObject.amount, 10);
     });
   });
+
+  describe('Calculate hash', function() {
+    it('should correct calculate the SHA256', function() {
+      txObject = new Transaction('a1', 'b1', 10);
+      txObject.timestamp = 1;
+
+      assert.strict.equal(
+        txObject.calculateHash(),
+
+        // Output of SHA256(a1b1101)
+        '21894bb7b0e56aab9eb48d4402d94628a9a179bc277542a5703f417900275153'
+      );
+    });
